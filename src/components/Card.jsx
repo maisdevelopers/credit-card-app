a3b8c00d0da7eb47651cba69cc6efc2cf7dffebc
Junior Alves <developersmais@gmail.com>
2021-02-24 21:38:47 -0300

    Add usestate hook

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,61 @@
-import React from 'react'
-import './Card.css'
+import React, { useState } from 'react';
+import './Card.css';
+
 
 export default props => {
+    const [number, setNumber] = useState('**** **** **** ****');
+    const [name, setName] = useState('Nome Impresso');
+    const [date, setDate] = useState('00/00');
+
     return (
-        <div className="card card__amex">
-            <div className="flag">
-                <button className="card__btn">Editar</button>
-                <span className="flag__image amex"></span>
-            </div>
-
-            <div className="card__number">
-                <span className="text">4582 9436 4384 4345</span>
-            </div>
-
-            <div className="card__info">
-                <div className="card__info--name">
-                    <span className="title">Nome</span>
-                    <span className="text small">Junior A Alves</span>
+        <>
+            <div className="card edit card__master">
+                <div className="flag">
+                    <button className="card__btn">Editar</button>
+                    <span className="flag__image master"></span>
                 </div>
 
-                <div className="card__info--date">
-                    <span className="title">Validade</span>
-                    <span className="text small">07/21</span>
+                <div className="card__number">
+                    <span className="text">{ number }</span>
+                </div>
+
+                <div className="card__info">
+                    <div className="card__info--name">
+                        <span className="title">Nome</span>
+                        <span className="text small">{ name }</span>
+                    </div>
+
+                    <div className="card__info--date">
+                        <span className="title">Validade</span>
+                        <span className="text small">{ date }</span>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div className="form active">
+                <div className="form__content">
+
+                    <h2 className="title">Edite seu cartão de crédito</h2>
+                    <div className="form__control">
+                        <input type="tel" maxLength="19" onChange={e => setNumber(e.target.value)} placeholder="Número do cartão"/>
+                    </div>
+            
+                    <div className="form__control">
+                        <input type="text" onChange={e => setName(e.target.value)} placeholder="Nome impresso no cartão"/>
+                    </div>
+
+                    <div className="form__group">
+                        <div className="form__control">
+                            <input type="text" maxLength="5" value={date} onChange={e => setDate(e.target.value)} />
+                        </div>
+                        <div className="form__control">
+                            <input type="number" placeholder="CVV"/>
+                        </div>
+                    </div>
+                    
+                    <button className="btn">Salvar</button>
+                </div>
+            </div>
+        </>
     )
 }
